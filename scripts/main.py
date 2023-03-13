Units for insulin rate, openaps fix and shell fix for linux

scripts/main.py
@@ -11,11 +11,10 @@ if __name__ == "__main__":
     load_dotenv()
 
     training_data = TrainingData("./data/data.csv")
-
     ga = GlucoseInsulinGeneticAlgorithm()
     constants = ga.run(training_data)
 
-    # constants = [0.006087855309129808, 0.8169421695707391, 0.2341139159982908, 0.7514639148751912, 0.003159062595706197, 0.00033439542508328923, 0.026713764597784406, 124, 0.21414131324524588, 0.993974927000483, 0.007151843859299123, 0.7580049521993947]
+    # constants = [0.006466690185883084, 0.9986775362706466, 0.35617346722058174, 0.23659927959841165, 0.003982946883520522, 3.9495416978785336e-05, 0.027624490511824584, 115, 1.0357696550666873e-05, 0.5536124833434388, 0.009252262930285915, 0.9768732305922168]
 
     s1 = Scenario(30, 70, 20, 120, 180, 40, []) # Low Crashing
     s2 = Scenario(50, 70, 0, 120, 180, 40, []) # Low Stable
@@ -46,19 +45,4 @@ if __name__ == "__main__":
     #     insulin = 20 + np.random.normal() * 10 if np.random.normal() > 0.0 else 0
 
     #     scenario = Scenario(carbs, blood_glucose, insulin, 120, 180, 40, [])
-    #     scenario.run(constants, profile, basal_profile, recorded_carbs, "./output_3.csv")
-
-    # constants = [0.006130878853835453, 0.46253313443648725, 0.905360146129958, 0.907250092652974, 0.0014680021770052676, 0.0001753511527067264, 0.05578541545495119, 118, 0.3353304947598008, 0.11723477928743364, 0.02342579713860593]
-
-    # model = Model(training_data.find_initial_values(), constants)
-
-    # for intervention in training_data.interventions:
-    #     model.add_intervention(intervention[0], intervention[1], intervention[2])
-
-    # try:
-    #     for i in range(1, (training_data.timesteps - 1) * 5 + 1):
-    #         model.update(i)
-    # except:
-    #     raise Exception("Model learning failed")
-    
-    # model.plot()
+    #     scenario.run(constants, recorded_carbs, "./output_1.csv")